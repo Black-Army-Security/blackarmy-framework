bac30ea345f42690fde1be6bf009966f51ce313f
n0kk <n0kk@cyberforcesec.com.br>
2024-11-25 16:34:10 -0300

    Workaround to resolve git issues.

--- commands/webdiscover.py
+++ commands/webdiscover.py
@@ -18,15 +18,17 @@ def webdiscover(target_url, wordlist):
     safe_target = target_url.replace("://", "_").replace("/", "_")
 
     # Ensure the /var/tmp directory exists
-    output_dir = '/var/tmp'
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    webdiscover_scans_path = '/usr/share/blackarmy-framework/scans/webdiscover_scans/'
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists(webdiscover_scans_path):
+        os.makedirs(webdiscover_scans_path)
 
     # Create output files
-    waf_output_file = f'{output_dir}/waf_{safe_target}_output.txt'
-    robots_output_file = f'{output_dir}/robots_{safe_target}_output.txt'
-    webscan_output_file = f'{output_dir}/webscan_{safe_target}_output.txt'
-    dirbruteforce_output_file = f'{output_dir}/dirbruteforce_{safe_target}_output.txt'
+    waf_output_file = f'{webdiscover_scans_path}/waf_{safe_target}_output.txt'
+    robots_output_file = f'{webdiscover_scans_path}/robots_{safe_target}_output.txt'
+    webscan_output_file = f'{webdiscover_scans_path}/webscan_{safe_target}_output.txt'
+    dirbruteforce_output_file = f'{webdiscover_scans_path}/dirbruteforce_{safe_target}_output.txt'
 
     # Commands
     waf_enum = f'wafw00f {target_url} > {waf_output_file}'
@@ -46,3 +48,4 @@ def webdiscover(target_url, wordlist):
     print(f'Running Directory Brute Force on: {target_url}')
     os.system(dir_brute_enum)
     print(f'The Directory Brute Force scan has finished. Results saved in: {dirbruteforce_output_file}')
+    print(f'All scans have finished. Results saved in: {webdiscover_scans_path}')
